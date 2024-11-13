d647cb04930f43703e4ee937ce1caa1abb8f6b40
sobir-swe <sobirjon.swe@gmail.com>
2024-11-13 10:51:29 +0500

    Add Image model for image data handling

---
+++ app/Models/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = [
+        'url',
+        'width',
+        'height',
+        'text',
+        'path'
+    ];
+}
