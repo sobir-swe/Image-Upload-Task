e4398aac9212d23129f8dd813294cf79140423ab
sobir-swe <sobirjon.swe@gmail.com>
2024-11-13 10:51:29 +0500

    Update routes for image upload feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('image_upload');
 });
+
+Route::post('/images', [ImageController::class, 'store']);
+
+Route::get('/images', [ImageController::class, 'index']);
