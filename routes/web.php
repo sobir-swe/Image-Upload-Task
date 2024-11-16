54c3496c645e6f25e6f47ab943a5371b2d2776f0
sobir-swe <sobirjon.swe@gmail.com>
2024-11-17 03:52:33 +0500

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('image_upload');
 });
-
-Route::post('/images', [ImageController::class, 'store']);
-
-Route::get('/images', [ImageController::class, 'index']);
-
-Route::get('/images', [ImageController::class, 'getImages']);
