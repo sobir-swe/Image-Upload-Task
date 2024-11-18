dc0259e0ade3b4dd01a5171460e0491cdaff1418
sobir-swe <sobirjon.swe@gmail.com>
2024-11-18 17:40:51 +0500

    feat: add authentication views, components, and dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p>{{ __("Download via image address") }}</p>
+                    <a href="/images" class="text-blue-500 hover:underline">Click here to download</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p>{{ __("Download through the site!") }}</p>
+                    <a href="/sites" class="text-blue-500 hover:underline">Click here to download</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
