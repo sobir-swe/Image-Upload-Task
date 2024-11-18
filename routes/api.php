0e21fbecd8417b5047ecb2a52fccc4272a77b530
sobir-swe <sobirjon.swe@gmail.com>
2024-11-18 17:40:30 +0500

    chore: update routing configurations in api.php and web.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/images', ImageController::class);
+Route::get('/gallery', [ImageController::class, 'showGallery']);
+
+Route::resource('/sites', SiteController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,27 @@
 <?php
 
-use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/images', function () {
     return view('image_upload');
 });
+
+Route::get('/sites', function () {
+    return view('site_upload');
+});
+require __DIR__.'/auth.php';
