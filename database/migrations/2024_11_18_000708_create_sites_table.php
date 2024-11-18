e757348fe7440994e995a13d8f821263ee06cb31
sobir-swe <sobirjon.swe@gmail.com>
2024-11-18 18:07:13 +0500

    fix: update sites table

--- database/migrations/2024_11_18_000708_create_sites_table.php
+++ database/migrations/2024_11_18_000708_create_sites_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer('width');
             $table->integer('height');
             $table->string('text')->nullable();
+
             $table->timestamps();
         });
     }
