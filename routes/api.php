3492de145abd5261227cd954f7fd0506e2b771df
sobir-swe <sobirjon.swe@gmail.com>
2024-11-19 21:34:46 +0500

    feat: update routes for image and site functionality

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('/images', ImageController::class);
 
-//Route::resource('/sites', SiteController::class);
-
-Route::post('/sites/upload', [SiteController::class, 'upload']);
+Route::resource('/sites', SiteController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,4 +26,6 @@ Route::get('/images', function () {
 Route::get('/sites', function () {
     return view('site_upload');
 });
+
+
 require __DIR__.'/auth.php';
